a372fc8ecd6d48cb477f6b5f145e1bb797ee7ed7
Gloria Ukaoma <gukaoma@gmail.com>
2018-01-16 03:47:11 +0000

    more stories and pages

--- application-controller.rb
+++ application-controller.rb
@@ -12,24 +12,29 @@ get '/input' do
   erb :input
 end
 
+get '/about' do 
+  erb :about
+end
+
 get '/updates' do 
   erb :updates
 end
 
-# post '/getstory' do
-#     info = params[:input]
-#     @name = story(info)[:name]
-#     @book = story(info)[:book]
-#     @color = story(info)[:color]
-#     @car = story(info)[:car]
-#     erb :results
-# end
+get '/faq' do 
+  erb :faq
+end
 
 post '/getstory' do
+    tem = params[:template]
+    @title = story(tem)[:title]
     @name = params[:name]
     @book = params[:book]
     @color = params[:color]
     @car = params[:car]
+    @part1 = story(tem)[:story][0]
+    @part2 = story(tem)[:story][1]
+    @part3 = story(tem)[:story][2]
+    @part4 = story(tem)[:story][3]
     erb :results
 end
 
--- generator.rb
+++ generator.rb
@@ -1,8 +1,32 @@
-# def story(name, book, color, car)
-# input = {
-#   :name => name,
-#   :book => book,
-#   :color => color,
-#   :car => car
-# }
-# end
+def story(template)
+    if template == "adventure"
+        adventure = {
+            :title => "The Harrowing Adventures Of",
+            :story => ["My name is", "I read", "My favorite color is", "I drive a"]
+        }
+        
+    elsif template == "sci_fi"
+        sci_fi = {
+            :title => "Heaven On Mars",
+            :story => ["My name is", "I read", "My favorite color is", "I drive a"]
+        }
+        
+    elsif template == "angst"
+        angst = {
+            :title => "Forget About Me",
+            :story => ["My name is", "I read", "My favorite color is", "I drive a"]
+        }
+        
+        elsif template == "fantasy"
+        fantasy = {
+            :title => "Open Your Eyes",
+            :story => ["My name is", "I read", "My favorite color is", "I drive a"]
+        }
+        
+        elsif template == "contempory"
+        contempory = {
+            :title => "California Love",
+            :story => ["My name is", "I read", "My favorite color is", "I drive a"]
+        }
+    end
+end
